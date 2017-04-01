5075b34c85795481f54637f76b843fbbea37eea3
Robert Deans <robdeans@gmail.com>
2017-04-01 14:42:57 -0400

    adds podfile

---
+++ GeoAlarms/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeoAlarms
+//
+//  Created by Robert Deans on 4/1/17.
+//  Copyright © 2017 Robert Deans. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
